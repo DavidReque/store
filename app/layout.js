1e539a1e6a7761cca5b75ff57fdffd6e92969ee1
David Requeno <davidrequeno52@gmail.com>
2023-05-14 16:19:55 -0600

    next routes

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import '../styles/globals.css'
 import { Space_Grotesk as spaceGrotesk } from 'next/font/google'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
 
 const space = spaceGrotesk({
   weight: ['400', '700'],
@@ -14,7 +16,11 @@ export const metadata = {
 export default function RootLayout ({ children }) {
   return (
     <html lang='en'>
-      <body className={space.className}>{children}</body>
+      <body className={space.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
