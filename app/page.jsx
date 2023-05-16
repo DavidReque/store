508ef62c699fbe9025037e63e9ff8043a51c69af
David Requeno <davidrequeno52@gmail.com>
2023-05-15 22:09:42 -0600

    Chakra ui componenrs

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,14 @@
+'use client'
+import { ChakraBaseProvider } from '@chakra-ui/react'
+
 export default function Home () {
   return (
-    <div>
-      <main>
-        Home
-      </main>
-    </div>
+    <ChakraBaseProvider>
+      <div>
+        <main>
+          Home
+        </main>
+      </div>
+    </ChakraBaseProvider>
   )
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+'use client'
+
+import { Tooltip } from '@chakra-ui/react'
 
 export default function Header () {
   return (
-    <div>Header</div>
+    <header>
+      <Tooltip label="Hey, I'm here!" aria-label='A tooltip'>
+        Hover me
+      </Tooltip>
+    </header>
   )
 }
