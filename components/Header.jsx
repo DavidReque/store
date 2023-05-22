ee71b51cee07a1b050df9b8db13ccfe66c611478
David Requeno <davidrequeno52@gmail.com>
2023-05-21 20:50:13 -0600

    Logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,14 @@
 'use client'
 
 import SideBar from './SideBar'
+import Link from 'next/link'
 
 export default function Header () {
   return (
     <header className='h-[50px] flex justify-center items-center'>
+      <div>
+        <Link href='/'>Logo</Link>
+      </div>
       <SideBar />
     </header>
   )
