f659d25c500c806fcb244dde0fb9ff41ab8d4164
David Requeno <davidrequeno52@gmail.com>
2023-05-22 21:16:39 -0600

    svg

--- components/Header.jsx
+++ components/Header.jsx
@@ -7,7 +7,7 @@ import Link from 'next/link'
 
 export default function Header () {
   return (
-    <header className='h-[60px] flex justify-center items-center gap-2'>
+    <header className='h-[60px] flex justify-center items-center gap-3'>
       <div>
         <Link href='/'>Logo</Link>
       </div>
