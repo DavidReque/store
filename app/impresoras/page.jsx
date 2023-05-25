e01cb7766fdd7eddf815ed912026472c1422483a
David Requeno <davidrequeno52@gmail.com>
2023-05-24 22:24:45 -0600

    submenu

--- app/impresoras/page.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function impresoras () {
-  return (
-    <div>impresoras</div>
-  )
-}
--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,46 +1,87 @@
-'use client'
 import { useState } from 'react'
 import Link from 'next/link'
-import { Tooltip } from '@chakra-ui/react'
 
 const data = [
   { id: 1, name: 'Inicio', url: '/' },
-  { id: 2, name: 'Computadoras', url: '/computadoras' },
-  { id: 3, name: 'Celulares', url: '/celulares' },
-  { id: 4, name: 'Impresoras', url: '/impresoras' }
-]
-
-const subMenu = [
-  { id: 1, name: 'Mantenimiento', url: './computadoras/mantenimiento' },
-  { id: 2, name: 'Gamer', url: '/computadoras/gamer' },
-  { id: 3, name: 'accesorios', url: '/accesorios/computadoras' }
+  {
+    id: 2,
+    name: 'Computadoras',
+    url: '/computadoras',
+    subCategories: [
+      { id: 1, name: 'Mantenimiento', url: './computadoras/mantenimiento' },
+      { id: 2, name: 'Gamer', url: '/computadoras/gamer' },
+      { id: 3, name: 'Accesorios', url: '/computadoras/accesorios' }
+    ]
+  },
+  {
+    id: 3,
+    name: 'Celulares',
+    url: '/celulares',
+    subCategories: [
+      { id: 1, name: 'Fundas', url: './celulares/fundas' },
+      { id: 2, name: 'Auriculares', url: '/celulares/auriculares' },
+      { id: 3, name: 'Accesorios', url: '/celulares/accesorios' }
+    ]
+  },
+  {
+    id: 4,
+    name: 'Impresoras',
+    url: '/impresoras',
+    subCategories: [
+      { id: 1, name: 'Mantenimiento', url: './impresoras/mantenimiento' },
+      { id: 3, name: 'Accesorios', url: '/impresoras/accesorios' }
+    ]
+  }
 ]
 
 export default function Menu () {
-  const [subMenuOpen, setSubMenuOpen] = useState(false)
+  const [activeProduct, setActiveProduct] = useState(null)
 
-  const showSubMenu = () => {
-    setSubMenuOpen(!subMenuOpen)
+  const handleMouseEnter = (product) => {
+    setActiveProduct(product)
+  }
+
+  const handleMouseLeave = () => {
+    setActiveProduct(null)
   }
 
   return (
     <ul>
-      {data.map((products) => (
-        <div className='flex hover:bg-slate-100 h-[45px]' key={products.id}>
-          <Tooltip label={products.name} aria-label='A tooltip'>
-            <Link className='ml-3 mt-3 w-full' href={products.url}>
-              {products.name.toLocaleUpperCase()}
-            </Link>
-          </Tooltip>
-        </div>
+      {data.map((product) => (
+        <li
+          key={product.id}
+          className='flex hover:bg-slate-100 h-[45px]'
+          onMouseEnter={() => handleMouseEnter(product)}
+          onMouseLeave={handleMouseLeave}
+        >
+          <Link className='ml-3 mt-3 w-full' href={product.url}>
+            {product.name.toLocaleUpperCase()}
+          </Link>
+          <button>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={1.5}
+              stroke='currentColor'
+              className='w-6 h-6'
+            >
+              {activeProduct && activeProduct.id === product.id
+                ? (
+                  <path strokeLinecap='round' strokeLinejoin='round' d='M5 15l7-7 7 7' />
+                  )
+                : (
+                  <path strokeLinecap='round' strokeLinejoin='round' d='M19.5 8.25l-7.5 7.5-7.5-7.5' />
+                  )}
+            </svg>
+          </button>
+        </li>
       ))}
-      {showSubMenu && (
+      {activeProduct && activeProduct.subCategories && (
         <ul>
-          {subMenu.map((moreProducts) => (
-            <li key={moreProducts.id}>
-              <Link href={moreProducts.url}>
-                {moreProducts.name}
-              </Link>
+          {activeProduct.subCategories.map((category) => (
+            <li key={category.id}>
+              <Link href={category.url}>{category.name}</Link>
             </li>
           ))}
         </ul>
