fee0a0d9d119cb4fe4f8646b5da04dc1cb0f0e4e
David Requeno <davidrequeno52@gmail.com>
2023-05-25 21:28:42 -0600

    Popover

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Link from 'next/link'
+import { Popover } from '@headlessui/react'
 
 const data = [
   { id: 1, name: 'Inicio', url: '/' },
@@ -37,40 +38,61 @@ const data = [
 export default function Menu () {
   const [activeMenuItem, setActiveMenuItem] = useState(null)
 
-  const handleMouseEnter = (itemId) => {
-    setActiveMenuItem(itemId)
-  }
-
-  const handleMouseLeave = () => {
-    setActiveMenuItem(null)
+  const toggleSubMenu = (itemId) => {
+    setActiveMenuItem(activeMenuItem === itemId ? null : itemId)
   }
 
   return (
     <ul className='flex flex-col space-y-2'>
       {data.map((item, index) => (
-        <li
-          key={item.id}
-          className={`relative group ${index !== 0 ? 'mt-2' : ''}`}
-          onMouseEnter={() => handleMouseEnter(item.id)}
-          onMouseLeave={handleMouseLeave}
-        >
-          <Link href={item.url} className='font-bold text-lg hover:text-blue-500'>
-            {item.name.toLocaleUpperCase()}
-          </Link>
-          {item.subCategories && activeMenuItem === item.id && (
-            <div className='absolute left-full top-0 mt-[-2px] bg-white p-2 rounded-md shadow-md'>
-              {item.subCategories.map((subItem) => (
-                <div key={subItem.id}>
-                  <Link
-                    href={subItem.url}
-                    className='block py-1 px-2 hover:bg-blue-500 hover:text-white rounded-md'
+        <li key={item.id} className={`relative group ${index !== 0 ? 'mt-2' : ''}`}>
+          <Popover>
+            {({ open }) => (
+              <>
+                <Popover.Button
+                  className={`flex items-center font-bold text-lg hover:text-blue-500 focus:outline-none ${
+                    open ? 'text-blue-500' : ''
+                  }`}
+                  onClick={() => toggleSubMenu(item.id)}
+                >
+                  {item.name}
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    stroke='currentColor'
+                    className={`w-4 h-4 ml-1 transition-transform ${
+                      open ? 'transform rotate-180' : ''
+                    }`}
                   >
-                    {subItem.name}
-                  </Link>
-                </div>
-              ))}
-            </div>
-          )}
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth={2}
+                      d='M19 9l-7 7-7-7'
+                    />
+                  </svg>
+                </Popover.Button>
+                {item.subCategories && (
+                  <Popover.Panel
+                    className={`absolute left-0 mt-2 w-48 bg-white rounded-md shadow-md ${
+                      activeMenuItem === item.id ? 'block' : 'hidden'
+                    }`}
+                  >
+                    <ul className='py-1'>
+                      {item.subCategories.map((subItem) => (
+                        <li key={subItem.id}>
+                          <Link href={subItem.url} className='block py-1 px-2 hover:bg-blue-500 hover:text-white rounded-md'>
+                            {subItem.name}
+                          </Link>
+                        </li>
+                      ))}
+                    </ul>
+                  </Popover.Panel>
+                )}
+              </>
+            )}
+          </Popover>
         </li>
       ))}
     </ul>
