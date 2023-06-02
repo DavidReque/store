31cf4b247dfe8c4a8da86d10858e27e87f198888
David Requeno <davidrequeno52@gmail.com>
2023-06-01 21:21:49 -0600

    Search button responsive

--- components/Header/Search.jsx
+++ components/Header/Search.jsx
@@ -1,15 +1,76 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Search () {
+  const [showInput, setShowInput] = useState(false)
+
+  const toggleInput = () => {
+    setShowInput(!showInput)
+  }
+
   return (
     <div>
-      <label className='relative block'>
-        <span className='sr-only'>Search</span>
-        <span className='absolute inset-y-0 left-0 flex items-center pl-2'>
-          <svg className='h-5 w-5 fill-slate-300' viewBox='0 0 20 20' />
-        </span>
-        <input className='placeholder:italic placeholder:text-slate-400 block bg-white w-[225px] border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm' placeholder='Buscar producto...' type='text' name='search' />
-      </label>
+      <div className='lg:hidden'>
+        {!showInput
+          ? (
+            <button
+              className='flex items-center justify-center p-2'
+              onClick={toggleInput}
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='w-6 h-6'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z'
+                />
+              </svg>
+              <span className='sr-only'>Buscar</span>
+            </button>
+            )
+          : (
+            <div className='relative'>
+              <input
+                className='placeholder-italic placeholder-slate-400 block bg-white w-[225px] border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm'
+                placeholder='Buscar producto...'
+                type='text'
+                name='search'
+              />
+              <button
+                className='absolute top-0 right-0 flex items-center justify-center p-2'
+                onClick={toggleInput}
+              >
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                  strokeWidth={1.5}
+                  stroke='currentColor'
+                  className='w-6 h-6'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    d='M6 18L18 6M6 6l12 12'
+                  />
+                </svg>
+              </button>
+            </div>
+            )}
+      </div>
+      <div className='hidden lg:block'>
+        <input
+          className='placeholder-italic placeholder-slate-400 block bg-white w-[225px] border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm'
+          placeholder='Buscar producto...'
+          type='text'
+          name='search'
+        />
+      </div>
     </div>
   )
 }
