bd756ec0e1a1ab580b1dea5759d47476a20877bc
David Requeno <davidrequeno52@gmail.com>
2023-06-22 22:28:35 -0600

    context

--- app/page.jsx
+++ app/page.jsx
@@ -3,13 +3,16 @@
 import Category from '@/components/Hero/Category'
 import HeroBanner from '@/components/Hero/HeroBanner'
 import ProductsList from '@/components/Products/ProductsList'
+import { CartProvider } from './CartContext'
 
 export default function Home () {
   return (
-    <main>
-      <HeroBanner />
-      <Category />
-      <ProductsList />
-    </main>
+    <CartProvider>
+      <main>
+        <HeroBanner />
+        <Category />
+        <ProductsList />
+      </main>
+    </CartProvider>
   )
 }
