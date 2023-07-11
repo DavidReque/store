6ef2522b0c8d22416bdae1b431a3989f290a0c08
David Requeno <davidrequeno52@gmail.com>
2023-07-10 20:47:47 -0600

    styles

--- app/carrito/Total.jsx
+++ app/carrito/Total.jsx
@@ -14,7 +14,12 @@ export default function Total ({ cartItems, quantities }) {
 
   return (
     <div className='text-center mt-4'>
-      <h2 className='font-bold'>Total: ${total.toFixed(2)}</h2>
+      <h2 className='font-bold text-lg sm:text-xl md:text-2xl mb-2'>Total a pagar</h2>
+      <div className='bg-gray-100 rounded-md p-4 inline-block'>
+        <span className='text-2xl sm:text-3xl md:text-4xl font-bold text-gray-800'>
+          ${total.toFixed(2)}
+        </span>
+      </div>
     </div>
   )
 }
