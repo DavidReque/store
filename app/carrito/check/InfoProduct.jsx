338c3c803f6612f8c4187fab55916bae823f602d
David Requeno <davidrequeno52@gmail.com>
2023-07-23 18:52:38 -0600

    Resumen compra

--- app/carrito/check/InfoProduct.jsx
+++ app/carrito/check/InfoProduct.jsx
@@ -20,31 +20,29 @@ export default function InfoProduct () {
   const total = subTotal + shippingCost
 
   return (
-    <div>
+    <div className='bg-white rounded-lg p-6 shadow-md'>
       <h2 className='text-2xl font-semibold mb-4'>Resumen de compra</h2>
-      <div className='border border-gray-300 rounded-lg p-4'>
-        <div className='mb-4'>
-          <h3 className='text-xl font-semibold mb-2'>Productos en el carrito:</h3>
-          <ul>
-            {cartItems.map((item) => (
-              <li key={item.id} className='mb-2'>
-                <span className='font-semibold'>{item.name}</span> x {quantities[item.id] || 1} - ${item.price.toFixed(2)}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className='mb-4'>
-          <h3 className='text-xl font-semibold'>Subtotal:</h3>
-          <p>${subTotal.toFixed(2)}</p>
-        </div>
-        <div className='mb-4'>
-          <h3 className='text-xl font-semibold'>Costo de envío:</h3>
-          <p>${shippingCost.toFixed(2)}</p>
-        </div>
-        <div className='mb-4'>
-          <h3 className='text-2xl font-semibold'>Total a pagar:</h3>
-          <p className='text-2xl font-bold'>${total.toFixed(2)}</p>
-        </div>
+      <div className='border-t border-gray-300 pt-4'>
+        <h3 className='text-lg font-semibold mb-2'>Productos en el carrito:</h3>
+        <ul>
+          {cartItems.map((item) => (
+            <li key={item.id} className='mb-2'>
+              <span className='font-semibold'>{item.name}</span> x {quantities[item.id] || 1} - ${item.price.toFixed(2)}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className='border-t border-gray-300 py-4'>
+        <h3 className='text-lg font-semibold'>Subtotal:</h3>
+        <p className='text-xl'>${subTotal.toFixed(2)}</p>
+      </div>
+      <div className='border-t border-gray-300 py-4'>
+        <h3 className='text-lg font-semibold'>Costo de envío:</h3>
+        <p className='text-xl'>${shippingCost.toFixed(2)}</p>
+      </div>
+      <div className='border-t border-gray-300 py-4'>
+        <h3 className='text-2xl font-semibold'>Total a pagar:</h3>
+        <p className='text-3xl font-bold'>${total.toFixed(2)}</p>
       </div>
     </div>
   )
