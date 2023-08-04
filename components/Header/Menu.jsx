2e1d966b2cfc863f029a67a027c640c7db40aec6
David Requeno <davidrequeno52@gmail.com>
2023-08-03 22:14:27 -0600

    menu text color

--- components/Header/Menu.jsx
+++ components/Header/Menu.jsx
@@ -11,43 +11,41 @@ export default function Menu () {
   }
 
   return (
-    <ul className='bg-gray-300 flex flex-col space-y-2 gap-1'>
+    <ul className='bg-blue-500 flex flex-col space-y-2 gap-1'>
       {data.map((item, index) => (
-        <li key={item.id} className={`relative group ${index !== 0 ? 'mt-2' : ''}`}>
+        <li key={item.id} className={`relative ${index !== 0 ? 'mt-2' : ''}`}>
           <Popover>
             {({ open }) => (
               <>
                 <Popover.Button
-                  className={`flex ml-4 mt-2 items-center font-bold text-lg hover:text-blue-500 focus:outline-none ${
+                  className={`flex ml-4 mt-2 items-center font-bold text-lg hover:text-white focus:outline-none ${
                     open ? 'text-blue-500' : ''
                   }`}
                   onClick={() => toggleSubMenu(item.id)}
                 >
-                  <Link className='mt-2' href={item.url}>{item.name.toLocaleUpperCase()}</Link>
-                  {item.id !== 1 && ( // Verificar si no es la opción 'Inicio'
+                  <Link className='mt-2 text-white' href={item.url}>
+                    {item.name.toLocaleUpperCase()}
+                  </Link>
+                  {item.id !== 1 && (
                     <svg
                       xmlns='http://www.w3.org/2000/svg'
                       fill='none'
                       viewBox='0 0 24 24'
                       stroke='currentColor'
-                      className={`w-4 h-4 mt-3 ml-4 transition-transform ${
-                        open ? 'transform rotate-180' : ''
-                      }`}
+                      className={`w-4 h-4 mt-3 ml-4 transition-transform ${open ? 'transform rotate-180' : ''}`}
                     >
                       <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M19 9l-7 7-7-7' />
                     </svg>
                   )}
                 </Popover.Button>
-                {item.subCategories && (
+                {item.subCategories && activeMenuItem === item.id && (
                   <Popover.Panel
-                    className={`absolute top-0 left-full mt-0 ml-2 w-[148px] bg-gray-300 rounded-md shadow-md ${
-                      activeMenuItem === item.id ? 'block' : 'hidden'
-                    }`}
+                    className='absolute top-0 left-full mt-0 ml-2 w-[148px] bg-gray-300 rounded-md shadow-md'
                   >
-                    <ul className='py-1'>
+                    <ul className='py-1 bg-blue-500 text-white'>
                       {item.subCategories.map((subItem) => (
-                        <li key={subItem.id}>
-                          <Link href={subItem.url} className='block py-1 px-2 hover:bg-blue-500 hover:text-white rounded-md'>
+                        <li key={subItem.id} className='hover:bg-white'>
+                          <Link href={subItem.url} className='block py-1 px-2 hover:text-blue-500 rounded-md'>
                             {subItem.name}
                           </Link>
                         </li>
